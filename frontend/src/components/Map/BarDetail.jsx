b413e0152fd44effd98cc507b9db462d8f42188a
shinhanseo <imkara1@naver.com>
2025-12-18 20:54:52 +0900

    지도 목록으로 되돌아가는 경로 수정

--- frontend/src/components/Map/BarDetail.jsx
+++ frontend/src/components/Map/BarDetail.jsx
@@ -60,10 +60,7 @@ export default function BarDetail() {
           <h2 className="text-2xl font-bold">{city}</h2>
         </div>
         <div className="mb-4">
-          <NavLink
-            to="/cocktails"
-            className="text-sm text-white/70 hover:font-bold"
-          >
+          <NavLink to="/bars" className="text-sm text-white/70 hover:font-bold">
             ← 목록으로
           </NavLink>
         </div>
@@ -84,10 +81,7 @@ export default function BarDetail() {
 
       {/* 상단 네비게이션 */}
       <div className="mb-4">
-        <NavLink
-          to="/cocktails"
-          className="text-sm text-white/70 hover:font-bold"
-        >
+        <NavLink to="/bars" className="text-sm text-white/70 hover:font-bold">
           ← 목록으로
         </NavLink>
       </div>
--- frontend/src/pages/CommunityPage.jsx
+++ frontend/src/pages/CommunityPage.jsx
@@ -4,7 +4,7 @@ import CommunityButton from "@/components/Community/CommunityButton";
 export default function Community() {
   return (
     <div className="w-screen min-h-screen flex flex-col items-center mt-4">
-      <ul className="w-full max-w-[1000px] text-white max-h-[80vh] border-white/10 rounded-lg">
+      <ul className="w-full max-w-[1000px] text-white max-h-[80vh] border-white/10 rounded-lg px-2 md:px-0">
         {/* 글 작성 버튼 */}
         <CommunityButton />
         {/* 글 헤더 및 목록 */}
